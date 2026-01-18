dbcf4a2d91cb496a0f771af95558ce40f9d8fb6b
Ebrahim Salehi <eeeee.bbbbb.iiiii@gmail.com>
2026-01-18 21:46:42 +0100

    feta: added python type

---
+++ python/images/py_003_type/type.py
@@ -0,0 +1,16 @@
+
+# 🐍 use type() to find the type
+# of any variable in Python 
+product_id = 1
+product_name = "Laptop"
+price = 12.9
+is_active = True
+
+print(type(product_id))  # <class 'int'>
+print(type(product_name))# <class 'str'>
+print(type(price))       # <class 'float'>
+print(type(is_active))   # <class 'bool'>
+
+# Are there any other ways 
+# to find variable types?
+# Please share it in comments
